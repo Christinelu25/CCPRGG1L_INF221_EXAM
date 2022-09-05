656adc8ff1530678b7266f0e8c299759112a3f7f
Christine Joy Lu <111473122+Christinelu25@users.noreply.github.com>
2022-09-05 10:18:22 +0800

    Create App.java

---
+++ Exam_1/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        
+                //Student  Class
+                Student Me = new Student();
+
+                Me.surname = "Lu";
+                Me.firstname = "Christine Joy";
+                Me.middleInitial = 'Q';
+                Me.dateOfBirth = "12/22/2003";
+                Me.studentNumber = 2022102511;
+                Me.studentEmailAddress = "lucq@students.national-u.edu.ph";
+                Me.iAmAwesome = false;
+                Me.sayMyStudentNumber();
+                Me.studentEmailAddress();
+                Me.iAmAwesome();
+
+
+
+
+
+
+    }
+}
